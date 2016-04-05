0fb034b30ae4533054bbd8ce9920932b8bd760ec
Lucas Rangel Cezimbra <lucascezimbra@gmail.com>
2016-04-05 00:47:50 -0300

    Format audios in mp3

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pafy
+from pydub import AudioSegment
 
 url = raw_input('Digite a URL da musica: ')
 audio = pafy.new(url).getbestaudio()
-audio.download();
+file = audio.download();
+
+mp3_audio = AudioSegment.from_file(file, audio.extension)
+mp3_audio.export(audio.title + '.mp3', format='mp3')
