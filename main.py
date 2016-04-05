10be2bb462f9e629f9eaf2619a6952cb823d56e4
Lucas Rangel Cezimbra <lucascezimbra@gmail.com>
2016-04-05 12:43:55 -0300

    Add BeautiflSoup, Add encoding utf-8, Fix tag.lyrics using set()

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+#coding: utf-8
+
 import pafy
 from pydub import AudioSegment
 import eyed3
+from bs4 import BeautifulSoup
+import requests
 
-url = raw_input('Digite a URL da musica: ')
+url = raw_input('Digite a URL da música: ')
 audio = pafy.new(url).getbestaudio()
 print(audio.title)
 file = audio.download();
@@ -16,5 +20,10 @@ mp3.tag.title = unicode(raw_input('Titulo: '))
 mp3.tag.artist = unicode(raw_input('Artista: '))
 mp3.tag.genre = unicode(raw_input('Genero: '))
 mp3.tag.album = unicode(raw_input('Album: '))
-#mp3.tag.lyrics = unicode(raw_input('Letra: '))
+mp3.tag.lyrics.set(unicode(raw_input('Letra: ')))
 mp3.tag.save()
+
+r = requests.get('https://www.letras.mus.br/rael-da-rima/ela-me-faz/')
+soup = BeautifulSoup(r.text)
+print soup.find(itemprop='description').find_all('p')
+print soup.find_all('h1')[1].string
