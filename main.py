3637652ea932fe6ea530684896364abe8b12edde
Lucas Rangel Cezimbra <lucascezimbra@gmail.com>
2016-04-08 00:25:34 -0300

    Add Vagalume API

--- main.py
+++ main.py
@@ -3,8 +3,7 @@
 import pafy
 from pydub import AudioSegment
 import eyed3
-from bs4 import BeautifulSoup
-import requests
+from vagalume import lyrics
 
 url = raw_input('Digite a URL da música: ')
 audio = pafy.new(url).getbestaudio()
@@ -15,15 +14,16 @@ print()
 mp3_audio = AudioSegment.from_file(file, audio.extension)
 mp3_audio.export(audio.title + '.mp3', format='mp3')
 
+
+titulo = raw_input('Titulo: ')
+artista = raw_input('Artista: ')
+
 mp3 = eyed3.load(audio.title + ".mp3")
-mp3.tag.title = unicode(raw_input('Titulo: '))
-mp3.tag.artist = unicode(raw_input('Artista: '))
+mp3.tag.title = unicode(titulo)
+mp3.tag.artist = unicode(artista)
 mp3.tag.genre = unicode(raw_input('Genero: '))
 mp3.tag.album = unicode(raw_input('Album: '))
-mp3.tag.lyrics.set(unicode(raw_input('Letra: ')))
+letra = lyrics.find(artista, titulo).song.lyric
+mp3.tag.lyrics.set(unicode(letra))
 mp3.tag.save()
 
-r = requests.get('https://www.letras.mus.br/rael-da-rima/ela-me-faz/')
-soup = BeautifulSoup(r.text)
-print soup.find(itemprop='description').find_all('p')
-print soup.find_all('h1')[1].string
